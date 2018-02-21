e7afe109ea4b1b500d2776770b7eb3a035c07ebc
gbalazs <gbalazs@localhost>
2018-02-21 16:46:18 +0100

    Spring profiles. SQL script. Info txt

---
+++ etc/db.sql
@@ -0,0 +1,3 @@
+create table cars (id int auto_increment, plate_number varchar(255), type varchar(255), primary key(id));
+insert into cars (plate_number, type) values ('ABC-123', 'Trabant');
+insert into cars (plate_number, type) values ('XXX-999', 'Porsche');
