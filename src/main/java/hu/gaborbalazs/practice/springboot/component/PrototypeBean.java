5db5d8a693cf4b32db8f4a1f4ec2bd3e1f92a730
gbalazs <gbalazs@localhost>
2018-03-19 15:15:41 +0100

    Prototype

--- src/main/java/hu/gaborbalazs/practice/springboot/component/PrototypeBean.java
+++ src/main/java/hu/gaborbalazs/practice/springboot/component/PrototypeBean.java
@@ -1,5 +1,9 @@
 package hu.gaborbalazs.practice.springboot.component;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -8,4 +12,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class PrototypeBean {
 
+private final Logger LOGGER = LoggerFactory.getLogger(PrototypeBean.class);
+    
+    private int num;
+    
+    @PostConstruct
+    public void init() {
+        LOGGER.trace(">> init()");
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void increaseNum() {
+        num++;
+    }
 }
